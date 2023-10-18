6a53598cc887e21a2f81b78a242bd418ae3f0e40
VictorCodebase <victorkithinji@outlook.com>
2023-10-18 11:04:57 +0300

    (LinkedListCycle): method 1 complete

---
+++ LinkedListCycle/method1.py
@@ -0,0 +1,24 @@
+
+class Node:
+    def __init__(self, data=0, next=None) -> None:
+        self.data = data
+        self.next = next
+
+class Solution:
+    #On2 solution
+    def checkForCycle(self, head: Node):
+        if head is None:
+            return False
+        list = []
+        while head:
+            if head in list:
+                return True
+            list.append(head)
+            head = head.next
+        return False
+
+#print(Solution.checkForCycle(Solution, Node(1, Node(2, Node(3, Node(4, Node(5)))))))
+# Provide data input that has a cycle
+print(Solution.checkForCycle(Solution, Node(1, Node(2, Node(3, Node(4, Node(5)))))))
+
+        
---
+++ LinkedListCycle/method2.py
@@ -0,0 +1,6 @@
+'''
+Floyd's Cycle Detection Algorithm
+'''
+
+#Please Copilot let me do this on my own. 
+
